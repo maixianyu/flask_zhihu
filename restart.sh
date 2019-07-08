88248df8006792511d2ea88579c016c31cb8e2e2
maixianyu <maixianyu@qq.com>
2019-07-08 21:28:29 +0800

    add restart.sh

---
+++ restart.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+# 重启服务器
+cp /var/www/flask_zhihu/flask_zhihu.nginx /etc/nginx/sites-enabled/flask_zhihu
+cp /var/www/flask_zhihu/flask_zhihu.conf /etc/supervisor/conf.d/flask_zhihu.conf
+
+
+service supervisor restart
+service nginx restart
+
+
+echo 'succsss'
+echo 'ip'
+hostname -I
